1c80202905ede9aa29901499f9c473889d203d1f
Josiah Johnson <Thetiedyeguy42@gmail.com>
2023-06-29 13:12:29 -0700

    Create lists.kt

---
+++ lists.kt
@@ -0,0 +1,20 @@
+fun main() {
+    val solarSystem = mutableListOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
+    println(solarSystem.size)
+    println(solarSystem[2])
+    println(solarSystem.get(3))
+    println(solarSystem.indexOf("Earth"))
+    println(solarSystem.indexOf("Pluto"))
+    for (planet in solarSystem){
+        println(planet)
+    }
+    solarSystem.add("Pluto")
+    solarSystem.add(3, "Theia")
+    solarSystem[3] = "Future Moon"
+    println(solarSystem[3])
+    println(solarSystem[9])
+    solarSystem.removeAt(9)
+    solarSystem.remove("Future Moon")
+    println(solarSystem.contains("Pluto"))
+    println("Future Moon" in solarSystem)
+}
