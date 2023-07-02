63b0b55b270c096071fd7c1dfd22a0e2b76e1a25
Josiah Johnson <Thetiedyeguy42@gmail.com>
2023-07-02 12:38:55 -0700

    Create higher-order.kt

---
+++ higher-order.kt
@@ -0,0 +1,111 @@
+class Cookie(
+	val name: String,
+    val softBaked: Boolean,
+    val hasFilling: Boolean,
+    val price: Double
+)
+
+val cookies = listOf(
+	Cookie(
+    	name = "Chocolate chip",
+        softBaked = false,
+        hasFilling = false,
+        price = 1.69
+    ),
+    Cookie(
+    	name = "Banana Walnut",
+        softBaked = true,
+        hasFilling = false,
+        price = 1.49
+    ),
+    Cookie(
+    	name = "Vanilla Creme",
+        softBaked = false,
+        hasFilling = true,
+        price = 1.59
+    ),
+    Cookie(
+    	name = "Chocolate Peanut Butter",
+        softBaked = false,
+        hasFilling = true,
+        price = 1.49
+    ),
+    Cookie(
+    	name = "Snickerdoodle",
+        softBaked = true,
+        hasFilling = false,
+        price = 1.39
+    ),
+    Cookie(
+    	name = "Blueberry Tart",
+        softBaked = true,
+        hasFilling = true,
+        price = 1.79
+    ),
+    Cookie(
+    	name = "Sugar and Sprinkles",
+        softBaked = false,
+        hasFilling = false,
+        price = 1.39
+    )
+)
+
+fun main() {
+    // All higher-order functions with collections
+    // all of the examples include the forEach function.
+    
+    // Map
+    val fullMenu = cookies.map {
+        "${it.name} - $${it.price}"
+    }
+    println("Full menu:")
+    fullMenu.forEach {
+        println(it)
+    }
+    println("")
+    
+    // Filter
+    val softBakedMenu = cookies.filter {
+        it.softBaked
+    }
+    println("Soft Cookies:")
+    softBakedMenu.forEach{
+        println("${it.name} - $${it.price}")
+    }
+    println("")
+    
+    // GroupBy
+    val groupedMenu = cookies.groupBy {
+        it.softBaked
+    }
+    val softBaked = groupedMenu[true] ?: listOf()
+    val crunchyMenu = groupedMenu[false] ?: listOf()
+    println("Soft cookies:")
+    softBakedMenu.forEach {
+        println("${it.name} - $${it.price}")
+    }
+    println("Crunchy cookies:")
+    crunchyMenu.forEach {
+        println("${it.name} - $${it.price}")
+    }
+    println("")
+    
+    // Fold
+    val totalPrice = cookies.fold(0.0){
+        total, cookie ->
+        total + cookie.price
+    }
+    println("Total price: $${totalPrice}")
+    println("")
+    
+    // Sorted By
+    val alphabeticalMenu = cookies.sortedBy {
+        it.name
+    }
+    println("Alphabetical menu:")
+    alphabeticalMenu.forEach {
+        println(it.name)
+    }
+    println("")
+    
+}
